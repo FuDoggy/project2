827aa2db0004292bdf20244b814058c6dfd91db7
Kevin Miller <kmillergit@outlook.com>
2020-09-07 14:05:31 -0700

    created object from recipe form that can be posted to Drink table

--- public/assets/js/recipes.js
+++ public/assets/js/recipes.js
@@ -1,12 +1,26 @@
 $("#recipe-form").on("submit", function(event) {
     event.preventDefault();
-    console.log(event)
-    console.log("hi")
-    console.log($("#drink-name").val());
-
+    let alcoCheck = document.getElementById("drink-alcoholic");
     // this grabs the value of each field, since all inputs have a class of ".recipe-inputs"
+
+    let newDrinkObj = {};
     for (let i = 0, j = $(".recipe-inputs").length; i < j; i++) {
-        console.log($(".recipe-inputs").eq(i).val())
+        // get the value and id of each form input
+        let formInput = $(".recipe-inputs").eq(i).val()
+        let elementId = ($(".recipe-inputs").eq(i).attr("id"))
+
+        // slice off the "drink-" portion of the id
+        let databaseId = elementId.slice(6,)
+
+        // if the checkbox is checked, set the alcoholic value to true
+        // this true value can alternatively be accessed through 
+        if (databaseId === "alcoholic") { 
+            formInput = alcoCheck.checked 
+            console.log("changed")
+        }
+        // add the data to the newDrinkObj
+        newDrinkObj[databaseId] = formInput;
+        console.log(newDrinkObj);
     }
 
     // get the user email so we can associate the user with the stored recipe in the SQL database
@@ -18,4 +32,7 @@ $("#recipe-form").on("submit", function(event) {
     // pictures? videos? we need to decide what we are posting to the database. Probably we can't store user videos, or even pictions, it would be preferable just to have a link to them. Will there be a default picture for drinks? Will the recipes be the same table as our currently existing drink table? Will we have a separate recipe table, or will recipes just be listed under the user table?
 
     // Finally, the last step is $.post to the database
+    // $.post("api/drinks", {
+    //     name: "hi"
+    // })
 })
