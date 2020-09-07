2b465d8eecc1f4fcac6446dc1e87939679dd23f4
Kevin Miller <kmillergit@outlook.com>
2020-09-07 00:35:13 -0700

    basic recipe form

---
+++ public/assets/js/recipes.js
@@ -0,0 +1,16 @@
+$("#recipe-form").on("submit", function(event) {
+    event.preventDefault();
+    console.log(event)
+    console.log("hi")
+    console.log($("#drink-name").val());
+
+    // this grabs the value of each field, since all inputs have a class of ".recipe-inputs"
+    for (let i = 0, j = $(".recipe-inputs").length; i < j; i++) {
+        console.log($(".recipe-inputs").eq(i).val())
+    }
+
+    // next we have to validate the data - make sure fields are not null, etc.
+    // pictures? videos?
+
+    // next step is $.post to the database
+})
