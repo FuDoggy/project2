52b848fd19ed8bf84bdea32327cb94ae87a1449b
Kevin Miller <kmillergit@outlook.com>
2020-09-08 15:00:06 -0700

    api route for getting user entered recipes is working

--- public/assets/js/recipes.js
+++ public/assets/js/recipes.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
+    let userEmail = localStorage.getItem("express-bartender-userEmail");
+    let userId = localStorage.getItem("express-bartender-userId");
+
+
     $("#recipe-form").on("submit", function(event) {
         event.preventDefault();
         let alcoCheck = document.getElementById("drink-alcoholic");
-        let userEmail = localStorage.getItem("express-bartender-userEmail");
-        let userId = localStorage.getItem("express-bartender-userId");
         // this grabs the value of each field, since all inputs have a class of ".recipe-inputs"
         
         // Make sure that the user id exists.
@@ -37,13 +39,23 @@ $(document).ready(() => {
         // Associate the user ID with the drink object
         newDrinkObj["UserId"] = userId;
         // TODO next we have to validate the data - make sure fields are not null, etc. Sequelize queries will return errors and crash server if the errors are not properly handled
-        
+
         // TODO pictures? videos? we need to decide what we are posting to the database. Probably we can't store user videos, or even pictions, it would be preferable just to have a link to them. Will there be a default picture for drinks? Will the recipes be the same table as our currently existing drink table? Will we have a separate recipe table, or will recipes just be listed under the user table?
-        
+
         // TODO add categories, or dropdown, or checkbox for vodka, whiskey etc. 
-        
+
         // Finally, the last step is $.post the new drink object to the database
         console.log(newDrinkObj);
         $.post("/api/drinks/new", newDrinkObj);
+        
     });
+
+    $("#view-my-drinks").on("click", function() {
+        console.log("clicked");
+        queryUrl = "/api/drinks/user/" + userId
+        $.get(queryUrl).then((data) => {
+            console.log("data is")
+            console.log(data);
+        })
+    })
 });
