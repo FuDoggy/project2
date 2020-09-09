67e5c75f94f93eac4ddc565003e0a1287df18f30
Kevin Miller <kmillergit@outlook.com>
2020-09-09 16:26:03 -0700

    put route works

--- public/assets/js/recipes.js
+++ public/assets/js/recipes.js
@@ -29,59 +29,8 @@ $(document).ready(() => {
             userId = Number(userId);
         }
 
-        // Creating a drink object to add to the SQL database:
-        let newDrinkObj = {};
-        for (let i = 0, j = $(".recipe-inputs").length; i < j; i++) {
-            // get the value and id of each form input
-            let formInput = $(".recipe-inputs").eq(i).val();
-            let elementId = ($(".recipe-inputs").eq(i).attr("id"));
+        let newDrinkObj = createDrinkObj();
 
-            // slice off the "drink-" portion of the id string
-            let databaseId = elementId.slice(6,);
-
-            // if the "alcoholic" checkbox is checked, set the alcoholic value to true (this value would otherwise be "on")
-            // only allow the rum, whiskey etc checkboxes to be true if "alcoholic" is also true
-            switch (databaseId) {
-                case "alcoholic":
-                    formInput = alcoCheck.checked;
-                    break;
-                case "rum":
-                    alcoCheck.checked === true ? formInput = document.getElementById("drink-rum").checked : formInput = false;
-                    break;
-                case "whiskey":
-                    alcoCheck.checked === true ? formInput = document.getElementById("drink-whiskey").checked : formInput = false;
-                    break;
-                case "tequila":
-                    alcoCheck.checked === true ? formInput = document.getElementById("drink-tequila").checked : formInput = false;
-                    break;
-                case "vodka":
-                    alcoCheck.checked === true ? formInput = document.getElementById("drink-vodka").checked : formInput = false;
-                    break;
-                case "gin":
-                    alcoCheck.checked === true ? formInput = document.getElementById("drink-gin").checked : formInput = false;
-                    break;
-                default:
-                    break;
-            }
-
-            // add the data to the newDrinkObj
-            newDrinkObj[databaseId] = formInput;
-        }
-
-        // Associate the user ID with the drink object
-        newDrinkObj["UserId"] = userId;
-        // hardcode a placeholder image
-        newDrinkObj["thumbnail"] = "https://www.standard.co.uk/s3fs-public/thumbnails/image/2016/09/30/10/cocktails.jpg"
-        // TODO next we have to validate the data - make sure fields are not null, etc. Sequelize queries will return errors and crash server if the errors are not properly handled
-
-        // TODO pictures? videos? we need to decide what we are posting to the database. 
-        // Probably we can't store user videos, or even pictions, it would be preferable just to have a link to them. 
-        // Will there be a default picture for drinks? 
-        // Will the recipes be the same table as our currently existing drink table? 
-        // Will we have a separate recipe table, or will recipes just be listed under the user table?
-
-        // Finally, the last step is $.post the new drink object to the database
-        console.log(newDrinkObj);
         $.post("/api/drinks/new", newDrinkObj).then(() => {
             $(".save-indicator").slideToggle("slow", function() {
                 setTimeout(() => {
@@ -194,13 +143,75 @@ $(document).ready(() => {
 
             // add update request to update the database
             currentRecipeUpdateBtn.on("click", function() {
-                let queryUrl = `/api/drinks/${data.UserId}/${data.id}`
+                let newDrinkObj = createDrinkObj();
+                console.log("NOWNOW")
+    
+                console.log(newDrinkObj)
+                newDrinkObj["id"] = data.id;
+                let queryUrl = `/api/drinks`
                 $.ajax({
                     url: queryUrl,
-                    method: "PUT"
+                    method: "PUT",
+                    data: newDrinkObj
                 })
             })
         })
+    }
+
+    function createDrinkObj() {
+        // Creating a drink object to add to the SQL database:
+        let newDrinkObj = {};
+        for (let i = 0, j = $(".recipe-inputs").length; i < j; i++) {
+            // get the value and id of each form input
+            let formInput = $(".recipe-inputs").eq(i).val();
+            let elementId = ($(".recipe-inputs").eq(i).attr("id"));
+
+            // slice off the "drink-" portion of the id string
+            let databaseId = elementId.slice(6,);
+
+            // if the "alcoholic" checkbox is checked, set the alcoholic value to true (this value would otherwise be "on")
+            // only allow the rum, whiskey etc checkboxes to be true if "alcoholic" is also true
+            switch (databaseId) {
+                case "alcoholic":
+                    formInput = alcoCheck.checked;
+                    break;
+                case "rum":
+                    alcoCheck.checked === true ? formInput = document.getElementById("drink-rum").checked : formInput = false;
+                    break;
+                case "whiskey":
+                    alcoCheck.checked === true ? formInput = document.getElementById("drink-whiskey").checked : formInput = false;
+                    break;
+                case "tequila":
+                    alcoCheck.checked === true ? formInput = document.getElementById("drink-tequila").checked : formInput = false;
+                    break;
+                case "vodka":
+                    alcoCheck.checked === true ? formInput = document.getElementById("drink-vodka").checked : formInput = false;
+                    break;
+                case "gin":
+                    alcoCheck.checked === true ? formInput = document.getElementById("drink-gin").checked : formInput = false;
+                    break;
+                default:
+                    break;
+            }
+
+            // add the data to the newDrinkObj
+            newDrinkObj[databaseId] = formInput;
+        }
+
+        // Associate the user ID with the drink object
+        newDrinkObj["UserId"] = userId;
+        // hardcode a placeholder image
+        newDrinkObj["thumbnail"] = "https://www.standard.co.uk/s3fs-public/thumbnails/image/2016/09/30/10/cocktails.jpg"
+        // TODO next we have to validate the data - make sure fields are not null, etc. Sequelize queries will return errors and crash server if the errors are not properly handled
+
+        // TODO pictures? videos? we need to decide what we are posting to the database. 
+        // Probably we can't store user videos, or even pictions, it would be preferable just to have a link to them. 
+        // Will there be a default picture for drinks? 
+        // Will the recipes be the same table as our currently existing drink table? 
+        // Will we have a separate recipe table, or will recipes just be listed under the user table?
+
+        // Finally, the last step is $.post the new drink object to the database
+        return newDrinkObj;
     }
 });
 
